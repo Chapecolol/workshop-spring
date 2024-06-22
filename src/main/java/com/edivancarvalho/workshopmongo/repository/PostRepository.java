b9ac241ac9b66aaa107241f7c63910bfa01fdb97
edivan junior <edivanjuniorfreitas@gmail.com>
2024-06-22 20:30:41 -0300

    Consultas simples com @Query

--- src/main/java/com/edivancarvalho/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/edivancarvalho/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.edivancarvalho.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.edivancarvalho.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.edivancarvalho.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> findByTitle(String text);
 	
 }
