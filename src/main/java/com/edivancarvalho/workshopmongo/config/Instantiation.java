56fc24f9d47eb1951bfe64e7f7974c7dc12b28b1
edivan junior <edivanjuniorfreitas@gmail.com>
2024-06-22 17:59:49 -0300

    FEAT: Projeção de dados do autor

--- src/main/java/com/edivancarvalho/workshopmongo/config/Instantiation.java
+++ src/main/java/com/edivancarvalho/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.edivancarvalho.workshopmongo.domain.Post;
 import com.edivancarvalho.workshopmongo.domain.User;
+import com.edivancarvalho.workshopmongo.dto.AuthorDTO;
 import com.edivancarvalho.workshopmongo.repository.PostRepository;
 import com.edivancarvalho.workshopmongo.repository.UserRepository;
 
@@ -32,11 +33,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para o Rio de Janeiro", maria);
-		Post post2 = new Post(null, sdf.parse("18/05/2024"), "Bom dia!", "Desejo a todos um bom dia", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para o Rio de Janeiro", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("18/05/2024"), "Bom dia!", "Desejo a todos um bom dia", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
